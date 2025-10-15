459bfca205eedad00aceec63c5b44711a711197d
JarneAdam <jarne.adam@student.vives.be>
2025-10-15 10:30:01 +0200

    Added schema and made project

---
+++ Lab_4/Lab_4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab_4
+//
+//  Created by Jarne Adam on 15/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab_4/Lab_4/Lab_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_4App.swift
+//  Lab_4
+//
+//  Created by Jarne Adam on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
